5222cfe5939f8505127b21e5751c9b64cd3fe77d
Mickey Loreti <mloreti10@gmail.com>
2016-09-20 20:21:24 -0700

    Finish specs

--- w2d2/spec/dessert_spec.rb
+++ w2d2/spec/dessert_spec.rb
@@ -14,9 +14,13 @@ describe Dessert do
       expect(brownie.type).to eq("brownie")
     end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(100)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to eq([])
+    end
 
     it "raises an argument error when given a non-integer quantity" do
       expect { Dessert.new("cake", "tons") }.to raise_error(ArgumentError)
@@ -24,16 +28,36 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    before do
+      brownie.add_ingredient("sugar")
+    end
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.ingredients).to eq(["sugar"])
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    before do
+      brownie.add_ingredient("sugar")
+      brownie.add_ingredient("water")
+      brownie.mix!
+    end
+    let(:ingredients) { brownie.ingredients }
+    it "shuffles the ingredient array" do
+      expect(brownie.mix!).to_not eq(:ingredients)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    before do
+      brownie.eat(1)
+    end
+    it "subtracts an amount from the quantity" do
+      expect(brownie.quantity).to eq(99)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect {brownie.eat(101)}.to raise_error(RuntimeError)
+    end
   end
 end
