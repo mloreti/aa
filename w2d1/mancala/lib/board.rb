e357dfe22c8f0944800876fcf34778e94443cbf0
Mickey Loreti <mloreti10@gmail.com>
2016-09-22 00:04:21 -0700

    Finish mancala

--- w2d1/mancala/lib/board.rb
+++ w2d1/mancala/lib/board.rb
@@ -4,10 +4,9 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @cups = Array.new(14) { [] }
+    @cups = Array.new(14) { Array.new }
     @name1 = name1
     @name2 = name2
-    @current_player = @name1
     place_stones
   end
 
@@ -24,14 +23,26 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    # debugger
     in_hand = @cups[start_pos]
 
+    # empties cup
+    stones = @cups[start_pos]
     @cups[start_pos] = []
-    counter = start_pos + 1
 
-    in_hand.each do |stone|
-      @cups[counter] << stone
+    # distributes stones
+    counter = start_pos
+    until in_hand.empty?
       counter += 1
+      counter = 0 if counter > 13
+      # places stones in the correct current player's cups
+      if counter == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif counter == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[counter] << stones.pop
+      end
     end
     render
     next_turn(counter)
@@ -39,7 +50,13 @@ class Board
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-    @cups[ending_cup_idx].empty?
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -51,8 +68,18 @@ class Board
   end
 
   def cups_empty?
+    @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    case @cups[6].length <=> @cups[13].length
+    when -1
+      @name2
+    when 0
+      :draw
+    when 1
+      @name1
+    end
+
   end
 end
