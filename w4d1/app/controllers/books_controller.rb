56af5289429749e912b1d6f53b554a98302e7915
Mickey Loreti <mloreti10@gmail.com>
2016-10-02 18:12:11 -0700

    Finish hw

--- w4d1/app/controllers/books_controller.rb
+++ w4d1/app/controllers/books_controller.rb
@@ -1,18 +1,30 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
+    # this render is optional; if we don't explicitly render or redirect,
+    # rails magic will automatically look for a corresponding index view
+    # and render it
+    render :index
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
