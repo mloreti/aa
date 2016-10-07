2071e816a953b8f109b307da28a97ade54772be9
Mickey Loreti <mloreti10@gmail.com>
2016-10-07 09:49:31 -0700

    Delete files

--- w3d3/DemoProject/db/migrate/20160928062846_drop_houses_table.rb
+++
@@ -1,9 +0,0 @@
-class DropHousesTable < ActiveRecord::Migration
-  def up
-    drop_table :houses_tables
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- w4d3/Auth/db/migrate/20161005041503_add_user_index.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIndex < ActiveRecord::Migration[5.0]
-  def change
-    add_index :users, :username
-    add_index :users, :password_digest
-    add_index :users, :session_token
-  end
-end
