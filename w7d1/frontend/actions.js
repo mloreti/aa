9dc2eadc7da0885eaec43be5f4cebfe55d8e43bd
Mickey Loreti <mloreti10@gmail.com>
2016-10-25 20:16:29 -0700

    Update homeworks

---
+++ w7d1/frontend/actions.js
@@ -0,0 +1,9 @@
+const selectCurrency = (baseCurrency, rates)=>{
+  return {
+    type: "SWITCH_CURRENCY",
+    baseCurrency: baseCurrency,
+    rates: rates
+  }
+}
+window.selectCurrency = selectCurrency
+export default selectCurrency;
